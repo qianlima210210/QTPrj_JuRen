d230c5b934aecd83c0bd4c4ac04cdf1155e7605c
maqianli <qianlima210210@163.com>
2019-10-06 21:59:54 +0800

    ListViewc的增删改

--- ListView001/ui_widget.h
+++ ListView001/ui_widget.h
@@ -13,8 +13,10 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QListView>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
@@ -24,6 +26,11 @@ class Ui_Widget
 {
 public:
     QVBoxLayout *verticalLayout;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *delRow;
+    QPushButton *addRow;
+    QPushButton *updateThd;
     QListView *listView;
 
     void setupUi(QWidget *Widget)
@@ -35,6 +42,30 @@ public:
         verticalLayout->setSpacing(6);
         verticalLayout->setContentsMargins(11, 11, 11, 11);
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        widget = new QWidget(Widget);
+        widget->setObjectName(QStringLiteral("widget"));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        delRow = new QPushButton(widget);
+        delRow->setObjectName(QStringLiteral("delRow"));
+
+        horizontalLayout->addWidget(delRow);
+
+        addRow = new QPushButton(widget);
+        addRow->setObjectName(QStringLiteral("addRow"));
+
+        horizontalLayout->addWidget(addRow);
+
+        updateThd = new QPushButton(widget);
+        updateThd->setObjectName(QStringLiteral("updateThd"));
+
+        horizontalLayout->addWidget(updateThd);
+
+
+        verticalLayout->addWidget(widget);
+
         listView = new QListView(Widget);
         listView->setObjectName(QStringLiteral("listView"));
 
@@ -49,6 +80,9 @@ public:
     void retranslateUi(QWidget *Widget)
     {
         Widget->setWindowTitle(QApplication::translate("Widget", "QlistView \347\256\200\345\215\225\345\272\224\347\224\250https://blog.csdn.net/yifanmoon/article/details/80495293", Q_NULLPTR));
+        delRow->setText(QApplication::translate("Widget", "\345\210\240\351\231\244", Q_NULLPTR));
+        addRow->setText(QApplication::translate("Widget", "\346\267\273\345\212\240", Q_NULLPTR));
+        updateThd->setText(QApplication::translate("Widget", "\346\233\264\346\226\260\347\254\254\344\270\211\351\241\271", Q_NULLPTR));
     } // retranslateUi
 
 };
--- ListView001/widget.cpp
+++ ListView001/widget.cpp
@@ -3,7 +3,10 @@
 #include <QMessageBox>
 #include <QModelIndex>
 #include <QIcon>
+#include <QDebug>
+#include <QPushButton>
 #include <QListWidget>
+#include <QInputDialog>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -43,6 +46,7 @@ void Widget::init()
     strList.append("F");
     strList.append("G");
 
+     //1、循环添加数据
     int nCount = strList.size();
     for(int i = 0; i < nCount; i++)
     {
@@ -56,11 +60,65 @@ void Widget::init()
     ui->listView->setModel(itemModel);
     ui->listView->setFixedSize(200,300);
 
+    //设置滚动方式：ScrollPerPixel视图将一次滚动一个像素的内容；ScrollPerItem:视图将一次滚动一项内容
+    ui->listView->setVerticalScrollMode(QAbstractItemView::ScrollPerItem);
+
+    //指定默认选中的行号
+    QModelIndex currentIndex=itemModel->index(3,0);//默认选中索引
+    ui->listView->setCurrentIndex(currentIndex);
+
+    //添加行
     void (QListView::*clicked)(const QModelIndex&) = &QListView::clicked;
     connect(ui->listView, clicked, this, &Widget::showClick);
     //connect(ui->listView,SIGNAL(clicked(QModelIndex)),this,SLOT(showClick(QModelIndex)));
+    connect(ui->addRow, &QPushButton::clicked, this, [=](){
+        QStandardItem *item = new QStandardItem(QString("item%1").arg(itemModel->rowCount()));
+        QStandardItemModel *model = dynamic_cast<QStandardItemModel*>(ui->listView->model());
+        if(itemModel->rowCount() % 2 == 0){
+            //从后面添加
+            model->appendRow(item);
+        }else{
+            //从前面插入
+            model->insertRow(0, item);
 
+            //设置不同背景
+            QLinearGradient linearGrad(QPointF(0, 0), QPointF(200, 200));
+            linearGrad.setColorAt(0, Qt::darkGreen);
+            linearGrad.setColorAt(1, Qt::yellow);
+            QBrush brush(linearGrad);
+            item->setBackground(brush);
+        }
 
+    });
+
+    //删除行
+    connect(ui->delRow, &QPushButton::clicked, this, [=](){
+        QStandardItemModel*model=dynamic_cast<QStandardItemModel*>(ui->listView->model());
+        qDebug() << QStringLiteral("您删除了第%1项").arg(ui->listView->currentIndex().row());
+        model->removeRow(ui->listView->currentIndex().row());
+    });
+
+    //更新行
+    connect(ui->updateThd, &QPushButton::clicked, this, [=](){
+    int row = ui->listView->currentIndex().row();
+    if(row!=-1)//-1表示没有处于选中状态的item
+     {
+        QStandardItemModel*listmodel=dynamic_cast<QStandardItemModel*>(ui->listView->model());
+        QModelIndex index=listmodel->index(row, 0);
+
+        QString str=listmodel->data(index,Qt::DisplayRole).toString();//get data
+        bool ok;
+        QString text = QInputDialog::getText(this, tr("edit item"), tr("please edit item"), QLineEdit::Normal,
+        str, &ok);
+        if (ok && !text.isEmpty())
+        {
+            listmodel->setData(index,text,Qt::EditRole);
+         }
+     }else
+     {
+        QMessageBox::information(NULL,tr("information"),tr("please select a item"));
+     }
+    });
 }
 
 void Widget::showClick(QModelIndex index)
