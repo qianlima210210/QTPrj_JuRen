eaaff6575981977fa584af4d4392ccbd28d9d670
maqianli <qianlima210210@163.com>
2019-11-04 14:50:03 +0800

    C++学习地址

---
+++ QMLcolorMaker/colormaker.h
@@ -0,0 +1,60 @@
+﻿#ifndef COLORMAKER_H
+#define COLORMAKER_H
+
+#include <QObject>
+#include <QColor>
+#include <QDateTime>
+#include <QTimerEvent>
+
+class ColorMaker : public QObject
+{
+    Q_OBJECT
+    Q_ENUMS(GenerateAlgorithm)
+    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
+    Q_PROPERTY(QColor timeColor READ timeColor)
+
+
+public:
+    explicit ColorMaker(QObject *parent = 0);
+    ~ColorMaker();
+
+    enum GenerateAlgorithm{
+            RandomRGB,
+            RandomRed,
+            RandomGreen,
+            RandomBlue,
+            LinearIncrease
+        };
+
+    QColor color() const;
+    void setColor(const QColor & color);
+    QColor timeColor() const;
+
+
+
+    //让一个类能够进入 Qt 强大的元对象系统（meta-object system）中，只有使用元对象系统，
+    //一个类的某些方法或属性才可能通过字符串形式的名字来调用，才具有了在 QML 中访问的基础条件。
+    // Q_INVOKABLE 宏来修饰，就可以让该方法被元对象系统调用
+    Q_INVOKABLE GenerateAlgorithm algorithm() const;
+    Q_INVOKABLE void setAlgorithm(GenerateAlgorithm algorithm);
+
+signals:
+    void colorChanged(const QColor & color);
+    void currentTime(const QString &strTime);
+
+public slots:
+    void start();
+    void stop();
+
+protected:
+    void timerEvent(QTimerEvent *e);
+
+private:
+    GenerateAlgorithm m_algorithm;
+    QColor m_currentColor;
+    int m_nColorTimer;
+
+
+};
+
+#endif // COLORMAKER_H
---
+++ QMLcolorMaker/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "colormaker.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    qmlRegisterType<ColorMaker>("an.qt.myCPP", 1, 0, "ColorMaker");//注册 QML 类型
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
+}
